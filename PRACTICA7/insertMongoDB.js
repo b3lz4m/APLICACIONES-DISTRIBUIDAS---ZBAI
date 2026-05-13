37650a8311367915046f340bb13f68a8ab985409
b3lz4m <alebelzamo@gmail.com>
2026-05-13 14:12:24 -0400

    UPLOAD PRACTICA7

---
+++ PRACTICA7/insertMongoDB.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+let client, database, collection;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+async function connectDB() {
+    const usuario = encodeURIComponent(process.env.DB_USER);
+    const password = encodeURIComponent(process.env.DB_PASS);
+    const cluster = process.env.DB_CLUSTER;
+    const uri = `mongodb+srv://${usuario}:${password}@${cluster}/?appName=Cluster0`;
+
+    client = new MongoClient(uri);
+    await client.connect();
+    database = client.db("myDatabase");
+    collection = database.collection("recipes");
+}
+
+app.get("/", (req, res) => res.json({ message: "Server is running" }));
+
+// Punto 6: Recibe el arreglo 'recipes' como parámetro en el body
+app.post("/receipt/insert", async (req, res) => {
+    const recipes = req.body.recipes; 
+    let result = "";
+
+    try {
+        const insertManyResult = await collection.insertMany(recipes);
+        result = `${insertManyResult.insertedCount} documentos insertados correctamente.`;
+        console.log(result);
+    } catch (err) {
+        result = `Error al insertar: ${err}`;
+        console.error(result);
+    }
+    res.json({ result: result });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor escuchando en el puerto 3000");
+    connectDB().catch(console.error);
+});
